5c0ad30c37b636cd5158d7320cb12733582761e8
Nguyen Duc Phuong <phuongnd.bk@gmail.com>
2015-11-03 08:50:04 +0000

    Edit layouts

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
   end
 end
