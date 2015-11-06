678ef295704bf5f1c94cabf7c6aef433a38d12bd
Vu Van Trung <xuhaohanoi@gmail.com>
2015-11-06 09:28:57 +0000

    Conpleted friend function

--- db/migrate/20151106081613_create_friends.rb
+++ db/migrate/20151106081613_create_friends.rb
@@ -1,8 +1,9 @@
 class CreateFriends < ActiveRecord::Migration
-  def change
-    create_table :friends do |t|
-      t.integer :user_id
-      t.integer :friend_id
+ def change
+   create_table :friends do |t|
+     t.integer :user_id
+     t.integer :friend_id
+     t.boolean :accepted
 
       t.timestamps null: false
     end
