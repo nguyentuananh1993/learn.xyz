f70f511a15b6439dafd657321f9d612e8f6b367d
Vu Van Trung <xuhaohanoi@gmail.com>
2015-11-06 08:41:55 +0000

    Start friend_function

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,22 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :user_logs
+  
+  def status_with_friend_id
+    if friend = Friend.where(user_id: self.id, friend_id: friend_id).try(:first)
+      if friend.accepted
+        :be_accepted
+      else
+        :requested
+      end
+      elsif friend = Friend.where(user_id: friend_id, friend_id: self.id).try(:first)
+        if friend.accepted
+          :accepted
+        else
+          :be_requested
+        end
+        else
+          :nothing
+        end
+  end
 end
---
+++ db/migrate/20151106081613_create_friends.rb
@@ -0,0 +1,11 @@
+class CreateFriends < ActiveRecord::Migration
+  def change
+    create_table :friends do |t|
+      t.integer :user_id
+      t.integer :friend_id
+
+      t.timestamps null: false
+    end
+    add_index :friends, :friend_id
+  end
+end
