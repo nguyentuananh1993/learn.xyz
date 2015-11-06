e0d5419bebb0688a3db81a02d2d01be27afb645a
sontdhust <sontd.hust@gmail.com>
2015-11-06 08:59:51 +0000

    user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+        @is_me = @user.id == current_user.id ? true : false
+        if not @is_me
+            @status = current_user.status_with @user.id
+            @friend = Friend.where(user_id: current_user.id, friend_id: @user.id).try(:first) ||
+                Friend.where(user_id: @user.id, friend_id: current_user.id).try(:first)
+        end
+    end
+end
