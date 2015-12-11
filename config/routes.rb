da8a16ac281cdc028a0adf8132d8f6b5f0b908d1
anh.nguyen.intern <anh.nguyen.intern@ntq-solutions.com.vn>
2015-12-11 12:48:59 +0700

    change exams

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     root "static_pages#home"
   end
 
-  root "static_pages#home"
+  # root "static_pages#home"
   get 'contact' => "static_pages#contact"
   devise_for :users
 
